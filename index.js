650aba0a96ad2fa15381789a7053b6d6a698549d
Leonardo <leo_mc_inter@hotmail.com>
2022-03-10 18:19:21 -0300

    Retirando interpretador de json

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
