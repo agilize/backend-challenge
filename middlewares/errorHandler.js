c43e311e461a4ff4e7a584383d93df790a531042
Leonardo <leo_mc_inter@hotmail.com>
2022-03-10 18:27:18 -0300

    Criado middleware para tratamento de erros

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+module.exports = (err, _req, res, _next) => {
+  if (err.status) {
+    return res.status(err.status).json({ message: err.message });
+  }
+
+  console.log(err)
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
