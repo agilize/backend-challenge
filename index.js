53860733e38fd8e1e08d952e0766eeb0b797155c
Leonardo <leo_mc_inter@hotmail.com>
2022-03-10 17:25:37 -0300

    Apropriando código ao prettier

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const puppeteer = require('puppeteer');
 
 async function teste() {
-  const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
   await page.goto(
     'https://www.transparencia.gov.br/despesas/orgao?ordenarPor=orgaoSuperior&direcao=asc'
-  )
+  );
 
   const infosFromTable = await page.evaluate(() => {
     const tds = Array.from(document.querySelectorAll('#lista tr td'));
-    return tds.map(td => td.innerText);
-  })
+    return tds.map((td) => td.innerText);
+  });
 
   await browser.close();
   return infosFromTable;
 };
 
 teste().then((result) => {
-  const arraysOfInfos = result.filter(value => value !== 'Detalhar').reduce((resultArray, item, index) => {
-    const breakOfArray = Math.floor(index/7);
+  const arraysOfInfos = result
+    .filter((value) => value !== 'Detalhar')
+    .reduce((resultArray, item, index) => {
+      const breakOfArray = Math.floor(index / 7);
 
-    if (!resultArray[breakOfArray]) {
-      resultArray[breakOfArray] = [];
-    }
+      if (!resultArray[breakOfArray]) {
+        resultArray[breakOfArray] = []
+      }
 
-    resultArray[breakOfArray].push(item);
+      resultArray[breakOfArray].push(item);
 
-    return resultArray;
-  }, []);
+      return resultArray;
+    }, [])
   console.log(arraysOfInfos);
 });
