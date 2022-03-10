ba4e39e1bfef4df0f7843d89168621dd13119603
Leonardo <leo_mc_inter@hotmail.com>
2022-03-10 18:27:04 -0300

    Ajustando ambiente Node

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const errorHandler = require('./middlewares/errorHandler');
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(errorHandler);
