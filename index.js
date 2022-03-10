f6dbee4e467ea2e36be2df74ff0783b6156ec464
Leonardo <leo_mc_inter@hotmail.com>
2022-03-10 19:47:57 -0300

    Adicionada rota ao index

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { getInfosGovController } = require('./controllers/infosgovController');
 
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+
+app.get('/api/dados', getInfosGovController);
 
 const PORT = process.env.PORT || 3000;
 
