69190b5d67c5e1a0c85fee62e0f2fb52246e7db9
Leonardo <leo_mc_inter@hotmail.com>
2022-03-10 18:27:31 -0300

    Utilitário de dicionário com expressões de rota

---
+++ utils/dictionary.js
@@ -0,0 +1,11 @@
+module.exports = {
+  success: 200,
+  created: 201,
+  noContent: 204,
+  badRequest: 400,
+  unauthorized: 401,
+  notFound: 404,
+  conflict: 409,
+  unprocessableEntity: 422,
+  serverError: 500,
+};
