340bb5c31b875159f191aee59b577c3580effd1d
Leonardo <leo_mc_inter@hotmail.com>
2022-03-11 03:59:03 -0300

    Variável PORT de ambiente adicionada ao index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+require('dotenv').config();
 const { getInfosGovController } = require('./controllers/infosgovController');
 
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+const PORT = process.env.PORT || 8090;
 
 app.get('/api/dados', getInfosGovController);
-
-const PORT = process.env.PORT || 3000;
 
 app.use(errorHandler);
 
