a3ccc4b21e832af544a793e941767f8ab0625960
Erickson <erickoparaguassu@hotmail.com>
2022-03-15 19:49:44 -0300

    Get implementado e funcionando

--- src/index.js
+++ src/index.js
@@ -1,70 +1,97 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const knex = require("./bancodedados/conexao");
-const fs = require('fs');
-const { data } = require("cheerio/lib/api/attributes");
+
+
+
+const app = express();
+const rotas = express();
+app.use(express.json());
+app.use(rotas);
+
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
 
 
 const url = "https://www.transparencia.gov.br/despesas/orgao?ordenarPor=orgaoSuperior&direcao=asc";
-const searchFor = "";
+
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-    await page.goto(url);
+  await page.goto(url);
 
-    const result = await page.$$eval('table tr', rows => {
-        return Array.from(rows, row => {
-            
-            const mes = row.querySelectorAll('td:nth-child(2)');
-            const programaOr = row.querySelectorAll('td:nth-child(3)');
-            const acaoOR = row.querySelectorAll('td:nth-child(4)');
-            const valorEm = row.querySelectorAll('td:nth-child(5)');
-            const valorLi = row.querySelectorAll('td:nth-child(6)');
-            const valorPa = row.querySelectorAll('td:nth-child(7)');
-            const valorRe = row.querySelectorAll('td:nth-child(8)');
+  const result = await page.$$eval('table tr', rows => {
+    return Array.from(rows, row => {
 
-          let mes_ano = Array.from(mes, column => column.innerText).toString().trim();
-          let programa_orcamentario = Array.from(programaOr, column => column.innerText).toString().trim();
-          let acao_orcamentaria = Array.from(acaoOR, column => column.innerText).toString().trim();
-          let valor_empenhado = Array.from(valorEm, column => column.innerText).toString().trim();;
-          let valor_liquidado = Array.from(valorLi, column => column.innerText).toString().trim();
-          let valor_pago = Array.from(valorPa, column => column.innerText).toString().trim();
-          let valor_restos_a_pagar_pagos = Array.from(valorRe, column => column.innerText).toString().trim();
+      const mes = row.querySelectorAll('td:nth-child(2)');
+      const programaOr = row.querySelectorAll('td:nth-child(3)');
+      const acaoOR = row.querySelectorAll('td:nth-child(4)');
+      const valorEm = row.querySelectorAll('td:nth-child(5)');
+      const valorLi = row.querySelectorAll('td:nth-child(6)');
+      const valorPa = row.querySelectorAll('td:nth-child(7)');
+      const valorRe = row.querySelectorAll('td:nth-child(8)');
 
-          const obj = {
-              mes_ano, 
-              programa_orcamentario,
-              acao_orcamentaria, 
-              valor_empenhado, 
-              valor_liquidado, 
-              valor_pago, 
-              valor_restos_a_pagar_pagos
-            };
+      let mes_ano = Array.from(mes, column => column.innerText).toString().trim();
+      let programa_orcamentario = Array.from(programaOr, column => column.innerText).toString().trim();
+      let acao_orcamentaria = Array.from(acaoOR, column => column.innerText).toString().trim();
+      let valor_empenhado = Array.from(valorEm, column => column.innerText).toString().trim();;
+      let valor_liquidado = Array.from(valorLi, column => column.innerText).toString().trim();
+      let valor_pago = Array.from(valorPa, column => column.innerText).toString().trim();
+      let valor_restos_a_pagar_pagos = Array.from(valorRe, column => column.innerText).toString().trim();
 
-          return obj
+      const obj = {
+        mes_ano,
+        programa_orcamentario,
+        acao_orcamentaria,
+        valor_empenhado,
+        valor_liquidado,
+        valor_pago,
+        valor_restos_a_pagar_pagos
+      };
 
-            
-        })
-    });
+      return obj
 
 
-  
-  
-    try {
-      knex.insert(result).into('despesas').then(data => {
-        console.log(data);
-      })
-    }
-    catch (error) {
-      console.log("erro")
-    }
-console.log(result)
+    })
+  });
 
-    await page.waitForTimeout(3000);
 
-    await browser.close();
+  try {
+    await knex.insert(result).into('despesas').then(data => {
+      console.log(data);
+    })
+  }
+  catch (error) {
+    console.log("erro")
+  }
+
+
+  await page.waitForTimeout(3000);
+
+  await browser.close();
 })();
 
+const mostrardados = async (req, res) => {
+  try{
+      const showDados = await knex.select().from("despesas");
+    return res.status(200).json(showDados);
+    
+
+    } catch (error) {
+        return res.status(404).json({mensagem: error.message})
+    }
+  }
+
+
+rotas.get('/api/dados', mostrardados);
+app.listen(3000);
