b60f8237485550b1cd2837c6a61c6b2a4cb993e0
marcoswarmling <marcoswarmling15@gmail.com>
2022-03-15 18:49:53 -0300

    desistindo dos testes pois não consegui achar a solução para o include_path do php

--- src/dataBase/connection.php
+++ src/dataBase/connection.php
@@ -25,7 +25,6 @@ class Connection
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
-            
         } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
--- src/tests/UnitTest.php
+++
@@ -1,2 +0,0 @@
-<?php 
-use PHPUnit\Framework\TestCase;
