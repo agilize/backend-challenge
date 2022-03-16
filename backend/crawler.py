0aba5b956dafc85b21ee6a0384ab1604a8226100
Breno Rodrigues <brenorm@gmail.com>
2022-03-16 19:21:50 -0300

    refactor(fetch): substituição da função fetch(url, timeout=3) por scraper(url), com utilização de libs Selenium e BeautifulSoup4 para requisição da página e obtenção dos dados a serem raspados.

--- backend/crawler.py
+++ backend/crawler.py
@@ -1,21 +1,32 @@
 import time
-import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 URL_BASE = "https://www.transparencia.gov.br/despesas/orgao?ordenarPor\
     =orgaoSuperior&direcao=asc"
 
 
-def fetch(url, timeout=3):
-    try:
-        response = requests.get(url, timeout=timeout)
-        response.raise_for_status()
-        time.sleep(1)
-    except (requests.HTTPError, requests.ReadTimeout):
-        return None
-    finally:
-        return response.text
+def scraper(url):
+    options = Options()
+    options.add_argument('--headless')
+
+    navegador = webdriver.Chrome(options=options)
+    navegador.get(url)
+    time.sleep(1)
+
+    page_content = navegador.page_source
+    full_loaded_html_content = BeautifulSoup(page_content, "html.parser")
+
+    table = full_loaded_html_content.find('table', id='lista')
+
+    headers = []
+    for title in table.find_all('th'):
+        headers.append(title.text)
+
+    return headers
 
 
 if __name__ == "__main__":
-    html_content = fetch(URL_BASE)
-    print(html_content)
+    table = scraper(URL_BASE)
+    print(table)
