e58dac44d6c58436ff4025409bbea28eb83cc503
Leonardo <leo_mc_inter@hotmail.com>
2022-03-20 16:17:55 -0300

    Função de scrapping dos dados chamada quando o servidor inicia

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const { getInfosGovController } = require('./controllers/infosgovController');
+const { createInfosGov } = require('./services/infosgovService');
+const errorHandling = require('./utils/errorHandling');
+const { badRequest } = require('./utils/dictionary');
 
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -9,6 +12,16 @@ const PORT = process.env.PORT || 8090;
 
 app.get('/api/dados', getInfosGovController);
 
+app.listen(
+  PORT,
+  () => console.log(`App running on port ${PORT}!`),
+  async () => {
+      await createInfosGov().catch(() => {
+        throw errorHandling(badRequest, 'Scrapping failed. Try restarting the server.');
+      });
+  }
+);
+
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
+module.exports = app;
